d7a524ec89c4fcfc979e736722ab7ca061f2841a
Riddick <danijel.galic@live.com>
2026-02-05 15:03:56 +0100

    Add `Math` class with `max` method for low-level numeric operations
    Introduced a `Math` class under the `FireHub\Core\Support\LowLevel` namespace, serving as a low-level wrapper for numeric and mathematical operations. Added a static `max` method to determine the highest value among multiple inputs using native PHP’s `max()` function. Comprehensive docblocks have been included to describe behavior, warn against type inconsistencies, and improve developer clarity.
    
    Modified the `PHPStan.yml` workflow file to dynamically read PHP versions and extensions via reusable variables (`${{ vars.PHP_VERSIONS }}` and `${{ vars.PHP_EXTENSIONS }}`). This change enhances flexibility and standardization for the CI pipeline.

---
+++ src/support/lowlevel/firehub.Math.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of the FireHub Project ecosystem
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2026 The FireHub Project - All rights reserved
+ * @license https://opensource.org/license/Apache-2-0 Apache License, Version 2.0
+ *
+ * @php-version 7.4
+ * @package Core\Support
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
+
+namespace FireHub\Core\Support\LowLevel;
+
+use function max;
+
+/**
+ * ### Math low-level proxy class
+ *
+ * Math utilities acting as thin proxies over native PHP numeric and mathematical functions.
+ * @since 1.0.0
+ */
+final class Math {
+
+    /**
+     * ### Find the highest value
+     * @since 1.0.0
+     *
+     * @template TValue
+     *
+     * @param TValue $value <p>
+     * Any comparable value.
+     * </p>
+     * @param TValue ...$values <p>
+     * Any comparable values.
+     * </p>
+     *
+     * @return TValue Value considered "highest" according to standard comparisons.
+     *
+     * @caution Be careful when passing arguments of different types because method can produce unpredictable results.
+     */
+    public static function max (mixed $value, mixed ...$values):mixed {
+
+        return max([$value, ...$values]);
+
+    }
+
+}
